5a8bc5af6c5a2ebcd2ceb18c9cb2b6bf878c39d3
tvoloshin <tvoloshin@phoenixit.ru>
2024-04-16 11:43:25 +0300

    Init

---
+++ src/test/java/com/atomskills/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atomskills.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
